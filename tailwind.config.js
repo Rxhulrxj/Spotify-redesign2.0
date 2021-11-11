738ec1eadb0e3c2a899d2eb1a5145d94e06ab0b5
Rahul Raj N <88202340+Rxhulrxj@users.noreply.github.com>
2021-11-11 22:06:08 +0530

    search bar session done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+    },
+    fontFamily: {
+      body: ["Architects Daughter", "cursive"],
+    },
   },
   variants: {
     extend: {},
+    scrollbar: ["rounded"],
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar-hide"),
+    require("tailwind-scrollbar"),
+  ],
+};
