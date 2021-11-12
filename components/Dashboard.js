0e1265df18eae93f2d303dd3b7dbed51a2221836
Rahul Raj N <88202340+Rxhulrxj@users.noreply.github.com>
2021-11-12 11:14:55 +0530

    working on spotifywebapi

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,9 @@
+import SpotifyWebApi from "spotify-web-api-node";
 import Body from "./Body";
 import Rightbar from "./Rightbar";
 import Sidebar from "./Sidebar";
 
+const spotifyApi = new SpotifyWebApi({});
 function Dashboard() {
   return (
     <main>
