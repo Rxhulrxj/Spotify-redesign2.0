682ebcadb25c0f8c3b101f63a6c6e30db8919b50
Rahul Raj N <88202340+Rxhulrxj@users.noreply.github.com>
2021-11-16 01:55:32 +0530

    working on body

--- components/Body.js
+++ components/Body.js
@@ -1,20 +1,113 @@
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import Poster from "./Poster";
 import Search from "./Search";
 
-function Body() {
+function Body({ chooseTrack, spotifyApi }) {
   const { data: session } = useSession();
   const { accessToken } = session;
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const [newReleases, setNewReleases] = useState([]);
+
   useEffect(() => {
     if (!accessToken) return;
-  }, []);
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  // Searching...
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    let cancel = false;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: track.album.images[0].url,
+            popularity: track.popularity,
+          };
+        })
+      );
+    });
+
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  // New Releases...
+  useEffect(() => {
+    if (!accessToken) return;
+
+    spotifyApi.getNewReleases().then((res) => {
+      setNewReleases(
+        res.body.albums.items.map((track) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: track.images[0].url,
+          };
+        })
+      );
+    });
+  }, [accessToken]);
+
   return (
     <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
-      <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4"></div>
+      <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
+        {searchResults.length === 0
+          ? newReleases
+              .slice(0, 4)
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))
+          : searchResults
+              .slice(0, 4)
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))}
+      </div>
+      <div className="flex gap-x-8 absolute min-w-full md:relative ml-6">
+        {/* genres */}
+        <div className="hidden xl:inline max-w-[270px]">
+          <h2 className="text-white font-bold mb-3">Genres</h2>
+          <div className="flex gap-x-2 gap-y-2.5 flex-wrap mb-3">
+            <div className="genre">Classic</div>
+            <div className="genre">House</div>
+            <div className="genre">Minimal</div>
+            <div className="genre">Hip-hop</div>
+            <div className="genre">Electronic</div>
+            <div className="genre">Chillout</div>
+            <div className="genre">Blues</div>
+            <div className="genre">Country</div>
+            <div className="genre">Techno</div>
+          </div>
+          <button className="btn">All Genres</button>
+        </div>
+        <div className="">
+          {/* Tracks */}
+          <h2 className="text-white font-bold mb-3">
+            {searchResults.length === 0 ? "New Releases" : "Tracks"}
+          </h2>
+        </div>
+      </div>
     </section>
   );
 }
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,14 +1,22 @@
+import { useRecoilState } from "recoil";
 import SpotifyWebApi from "spotify-web-api-node";
+import { playingTrackState } from "../atoms/playerAtom";
 import Body from "./Body";
 import Rightbar from "./Rightbar";
 import Sidebar from "./Sidebar";
 
-const spotifyApi = new SpotifyWebApi({});
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
 function Dashboard() {
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
   return (
     <main>
       <Sidebar />
-      <Body />
+      <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
       <Rightbar />
     </main>
   );
